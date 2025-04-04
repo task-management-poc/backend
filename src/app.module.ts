2b678b05566fb5a244089f07bf1c2bef7b0c43dd
jaanbaaz <jaanbaaz.akhtar@esmagico.in>
2025-04-04 21:22:55 +0530

    Task module import fix and cors error fix

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,17 +1,20 @@
 import { Module } from '@nestjs/common';
 import { SequelizeModule } from '@nestjs/sequelize';
-import { TaskModule } from './tasks/task.module';
 import { TaskModule } from './task/task.module';
+
+const env = require('dotenv');
+
+env.config();
 
 @Module({
   imports: [
     SequelizeModule.forRoot({
       dialect: 'postgres',
-      host: 'localhost',
-      port: 5432,
-      username: 'postgres',
-      password: 'password',
-      database: 'task_db',
+      host: process.env.DB_HOST,
+      port: parseInt(process.env.DB_PORT),
+      username: process.env.DB_USERNAME,
+      password: process.env.DB_PASSWORD,
+      database: process.env.DB_DATABASE,
       autoLoadModels: true,
       synchronize: true, // Set to false in production
     }),
--- src/main.ts
+++ src/main.ts
@@ -3,6 +3,14 @@ import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  
+  // Enable CORS
+  app.enableCors({
+    origin: 'http://localhost:3000', // Allow requests from frontend
+    methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
+    credentials: true, // Allow cookies and authentication headers
+  });
+
   await app.listen(3001);
 }
 bootstrap();
